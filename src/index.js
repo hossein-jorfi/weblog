71ce37de3f8d3718f21ba835ed605b0b4835ae7c
Hossein Jorfi <hosseinjorfi81@gmail.com>
2023-02-25 09:31:17 +0330

    Project Connected to graphQL

--- src/index.js
+++ src/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+
+const clinet = new ApolloClient({
+     uri: 'https://api-us-east-1-shared-usea1-02.hygraph.com/v2/clegjnd110sej01t3202uh15w/master',
+     cache: new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+     <ApolloProvider client={clinet}>
+          <App />
+     </ApolloProvider>
+);
